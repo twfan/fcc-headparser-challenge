dce26598252fc364c16326a7b14a02778142e54f
taufan <taufan@vecapital.asia>
2023-08-22 14:26:42 +0700

    challenge done

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res)=>{
+  const ipAddress = req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({
+    "ipaddress": ipAddress,
+    "language": language,
+    "software": software
+  })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
